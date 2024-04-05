7b8ec250d3519c09cd6332e7a61fd5192b87111a
Adarsh Jha <jhashailendra87@gmail.com>
2024-04-05 15:50:57 +0530

    all file changes and modify

--- Frontend/login/login.js
+++
@@ -1,7 +0,0 @@
-// Add this JavaScript code in a separate file named login.js
-
-document.getElementById('cancel-icon').addEventListener('click', function() {
-    // Redirect to another page or perform any desired action when the cancel icon is clicked
-    window.location.href = '../index.html'; // Replace 'index.html' with the desired destination
-  });
-  
